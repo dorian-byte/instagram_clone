fc7db47493f344be668670fbe97e83b0a8f36922
Ziming Liu <zimingliu@studio.local>
2022-06-07 12:12:48 -0700

    init: init ins project

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require("express");
-const app = express;
+const app = express();
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(
+    "Hello World!<div></div><a href='/users'>Users</a><div></div><a href='/page'>StyledPage</a>"
+  );
+});
+
+app.get("/users", (req, res) => {
+  res.send([
+    {
+      name: "John Doe",
+      age: 32,
+    },
+    {
+      name: "Jane Doe",
+      age: 30,
+    },
+  ]);
+});
+
+app.get("/page", (req, res) => {
+  res.send(
+    `<div>
+      <h1>StyledPage</h1>
+      <h2>title</h2>
+      <p>content</p>
+      <a href="/">Home</a>
+    </div>`
+  );
 });
 
 app.listen(PORT, () => {
